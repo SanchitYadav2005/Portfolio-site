49bfddeb2c4168aa39b1d39167e8d520f5ff46db
unknown <sanchit2005yudavanshi@gmail.com>
2022-12-09 20:33:59 +0530

    reconfigured views directory

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require("path");
 
 
 app.use(express.static('public'));
-app.use('view engine', 'ejs');
-app.use('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 
 app.get('/', (req,res)=>{
