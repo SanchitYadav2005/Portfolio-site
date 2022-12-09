fa2e7c318ed738d53a7332591db76854e4438ede
unknown <sanchit2005yudavanshi@gmail.com>
2022-12-09 20:30:12 +0530

    created views directory and configured it

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
 
 app.use(express.static('public'));
-
+app.use('view engine', 'ejs');
+app.use('views', path.join(__dirname, '/views'));
 
 
 app.get('/', (req,res)=>{
