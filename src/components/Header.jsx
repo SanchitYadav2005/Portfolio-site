730e136f63a3e72cc99c15f6aa37ddd55f014a03
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-04 08:26:40 +0530

    done with the structure of navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,39 @@
-function Header(){
-    return(
-        <h1>header</h1>
-    )
+import "../styles/navbar.css";
+import { motion } from "framer-motion";
+import "font-awesome/css/font-awesome.min.css";
+
+function Header() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <nav>
+        <h1 className="name">Sanchit Yadav</h1>
+        <ul className="list">
+          <li className="list-item">
+            <a className="list-item-link fa fa-instagram" id="instagram" href="#"></a>
+          </li>
+          <li className="list-item">
+            <a className="list-item-link fa fa-linkedin" id="linkedin" href="#"></a>
+          </li>
+          <li className="list-item">
+            <a className="list-item-link fa fa-twitter" id="twitter" href="#"></a>
+          </li>
+          <li className="list-item">
+            <a className="list-item-link" href="#">
+              <img
+                className="list-item-link-img"
+                src="src\assets\fiverr.png"
+                alt=""
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </motion.div>
+  );
 }
 
 export default Header;
