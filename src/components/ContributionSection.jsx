c10205ae9295198a320a4750624bc8308d633b4c
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-10 18:44:12 +0530

    installed octokit.js to use github api's

---
+++ src/components/ContributionSection.jsx
@@ -0,0 +1,7 @@
+function ContributionSection(){
+    return(
+        <h1>hello</h1>
+    )
+}
+
+export default ContributionSection;
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Skills from "./Skills";
+import ContributionSection from "./ContributionSection";
 
 function MainPage(){
     return(
         <>
             <Header/>
             <Skills/>
+            <ContributionSection/>
         </>
     )
 }
