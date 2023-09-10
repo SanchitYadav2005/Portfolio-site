5d7c099bd20e1d39100a99932f43aa2e3c6d02af
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-10 19:21:09 +0530

    not able to find the right api

--- src/components/ContributionSection.jsx
+++ src/components/ContributionSection.jsx
@@ -1,6 +1,20 @@
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+    auth: "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"
+  });
+
+const getData = async () => {
+    const result = await octokit.request('GET /repos/{owner}/{repo}/commits',{
+        owner: 'SanchitYadav2005',
+        repo: 'Portfolio-site'
+    })
+    console.log(result.data)
+}
+
 function ContributionSection(){
     return(
-        <h1>hello</h1>
+        <button onClick={getData}>click</button>
     )
 }
 
