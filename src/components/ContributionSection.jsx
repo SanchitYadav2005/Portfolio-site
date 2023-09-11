468242200b30dcf9da728ec02e2c71638b5bed80
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-11 05:46:09 +0530

    done

--- src/components/ContributionSection.jsx
+++ src/components/ContributionSection.jsx
@@ -1,10 +1,34 @@
+import GitHubCalendar from "react-github-calendar";
+import "../styles/github.css";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-
-function ContributionSection(){
-    
-return(
-        
-    )
+function ContributionSection() {
+    const [ref, isVisible] = useInView({ threshold: 0.5 });
+    const variants = {
+      visible: {
+        opacity: 1,
+        x: 0,
+      },
+      hidden: {
+        opacity: 0,
+        x: -100,
+      },
+    };
+  return (
+    <>
+    <motion.div
+     ref={ref}
+     variants={variants}
+     animate={isVisible ? "visible" : "hidden"}
+     transition={{ duration: 0.5, ease: "easeOut" }}>
+      <div className="container">
+        <h3>My Activity</h3>
+        <GitHubCalendar username="SanchitYadav2005" colorScheme="light" />
+      </div>
+      </motion.div>
+    </>
+  );
 }
 
 export default ContributionSection;
