c0025aafa5937890f68dd825eb9b577853fdebe6
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-11 06:51:18 +0530

    adding projects

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,7 +1,32 @@
-function ProjectSection(){
-    return(
-        <h2>display projects here</h2>
-    )
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+function ProjectSection() {
+  const [ref, isVisible] = useInView({ threshold: 0.5 });
+  const variants = {
+    visible: {
+      opacity: 1,
+      x: 0,
+    },
+    hidden: {
+      opacity: 0,
+      x: -100,
+    },
+  };
+  return (
+    <>
+      <div className="container">
+        <motion.div
+          ref={ref}
+          variants={variants}
+          animate={isVisible ? "visible" : "hidden"}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <h2>display projects here</h2>
+        </motion.div>
+      </div>
+    </>
+  );
 }
 
 export default ProjectSection;
