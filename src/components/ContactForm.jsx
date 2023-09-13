489a8990abf61a7a95dcb630a699a7185ddf4047
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-13 18:13:49 +0530

    seted the hook

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,7 +2,15 @@ import useInoutHook from "../hooks/useInoutHook";
 import "../styles/form.css";
 
 function ContactForm() {
-  const [value, handleChange, reset] = useInoutHook("");
+  const [
+    value1,
+    value2,
+    value3,
+    handleChange1,
+    handleChange2,
+    handleChange3,
+    reset,
+  ] = useInoutHook("");
   return (
     <>
       <h4 className="contact_me">Contact me</h4>
@@ -20,18 +28,32 @@ function ContactForm() {
             type="email"
             name="email"
             id="email"
-            value={value}
-            onChange={handleChange}
+            value={value1}
+            onChange={handleChange1}
           />
         </div>
         <div className="input-container">
           <label htmlFor="sub">Subject</label>
-          <input type="text" name="sub" id="sub" value={value} onChange={handleChange}/>
+          <input
+            type="text"
+            name="sub"
+            id="sub"
+            value={value2}
+            onChange={handleChange2}
+          />
         </div>
         <div className="input-container">
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="30" rows="10" value={value} onChange={handleChange}/>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            value={value3}
+            onChange={handleChange3}
+          />
         </div>
+        <button>Contact</button>
       </form>
     </>
   );
