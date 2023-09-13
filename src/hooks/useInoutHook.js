819946f42691f5fdd0dd020c22dccce102190ca9
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-13 18:13:29 +0530

    modified the hook for subject and email

--- src/hooks/useInoutHook.js
+++ src/hooks/useInoutHook.js
@@ -1,12 +1,30 @@
 import { useState } from "react";
 
-export default initialVal =>  {
-    const[value, setValue] = useState(initialVal);
-    const handleChange = e => {
-        setValue(e.target.value);
-    }
-    const reset = () =>{
-        setValue("")
-    }
-    return[value,handleChange,reset]
-}
+export default (initialVal) => {
+  const [value1, setValue1] = useState(initialVal);
+  const [value2, setValue2] = useState(initialVal);
+  const [value3, setValue3] = useState(initialVal);
+  const handleChange1 = (e) => {
+    setValue1(e.target.value);
+  };
+  const handleChange2 = (e) => {
+    setValue2(e.target.value);
+  };
+  const handleChange3 = (e) => {
+    setValue3(e.target.value);
+  };
+  const reset = () => {
+    setValue1("");
+    setValue2("");
+    setValue3("");
+  };
+  return [
+    value1,
+    value2,
+    value3,
+    handleChange1,
+    handleChange2,
+    handleChange3,
+    reset,
+  ];
+};
